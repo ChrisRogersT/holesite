b62c945e13cd85720f9db9b14ba427a61bab1b48
Chris <chrisrogerstezcat@gmail.com>
2022-08-30 18:03:51 -0700

    add a common layout and main app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Nav from '$lib/nav/Nav.svelte';
+</script>
+
+<Nav />
+<slot></slot>
+
+<svelte:head>
+    <style>
+        body{
+            margin: 0;
+            background-color:lightgoldenrodyellow;
+        }
+    </style>
+</svelte:head>
