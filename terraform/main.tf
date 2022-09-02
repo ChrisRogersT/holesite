aa049c2283ad002cc2c3f1d8a2c30b8a9f354cff
Chris <chrisrogerstezcat@gmail.com>
2022-09-01 18:51:33 -0700

    update to use the hashi corp official google storage bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,30 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "3.53.0"
+      version = "4.34.0"
     }
   }
 }
 
-locals {
-    project_id = "holesite"
-    region = "us-west1"
-    zone = "us-west1-b"
-}
-
 provider "google" {
-  project = local.project_id
-  region  = local.region
-  zone    = local.zone
+  project = "holesite"
+  region = "us-west1"
+  zone = "us-west1-b"
 }
 
-module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "~> 3.3"
-  project_id  = local.project_id
-  prefix = "bit_wiki"
-  names = ["entries"]
-  versioning = {
-    first = true
+resource "google_storage_bucket" "bit_wiki" {
+  name = "bit_wiki"
+  location = "us-west1"
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      num_newer_versions = 3
+      days_since_noncurrent_time = 14
+    }
+  }
+  versioning {
+    enabled = true
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "bucket_names_list" {
-    value = module.gcs_buckets.names_list
-}
-
 output "bucket_url" {
-    value = module.gcs_buckets.url
-}
-
-output "bucket_urls_list" {
-    value = module.gcs_buckets.urls_list
+    value = google_storage_bucket.bit_wiki.url
 }
