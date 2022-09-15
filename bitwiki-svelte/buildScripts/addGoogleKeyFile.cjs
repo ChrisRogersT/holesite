9e50546f88ceef81232ddafad65c210fea7c5bcc
Chris <chrisrogerstezcat@gmail.com>
2022-09-14 23:43:14 -0500

    some debuggin with vercel

--- bitwiki-svelte/buildScripts/addGoogleKeyFile.cjs
+++ bitwiki-svelte/buildScripts/addGoogleKeyFile.cjs
@@ -3,5 +3,5 @@ const fs = require('fs');
 const googleKeyFilePath = __dirname + '/../src/google_credentials_keyfile.json'
 console.log(googleKeyFilePath);
 fs.writeFile(googleKeyFilePath, process.env.GOOGLE_CREDENTIALS, (err)=>{
-    console.log(err);
+    if(err) console.log(err);
 })
